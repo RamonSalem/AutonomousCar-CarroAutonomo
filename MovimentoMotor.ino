d9e51466acf158153ae91138faf72b896e439d87
Ramon Salém <salemramon@protonmail.com>
2017-02-19 21:11:58 -0300

    First commit - Moviments - PonteH configs
    Functions related to movimentation of the autonomous car. We need to add
    the functions related to the sensors and the servo in the next deploy.

---
+++ MovimentoMotor.ino
@@ -0,0 +1,47 @@
+//Movimentos Básicos Motor 1 M1
+void trazM1(){
+    digitalWrite(IN1,HIGH);
+    digitalWrite(IN2,LOW);
+  }
+void frenteM1(){
+    digitalWrite(IN1,LOW);
+    digitalWrite(IN2,HIGH);
+  }
+void freioM1(){
+    digitalWrite(IN1,HIGH);
+    digitalWrite(IN2,HIGH);
+  }
+//Movimentos Básicos Motor 2 M2    
+void frenteM2(){
+    digitalWrite(IN3,HIGH);
+    digitalWrite(IN4,LOW);
+  }  
+void trazM2(){
+    digitalWrite(IN3,LOW);
+    digitalWrite(IN4,HIGH);
+  }  
+void freioM2(){
+    digitalWrite(IN3,HIGH);
+    digitalWrite(IN4,HIGH);
+  }
+//Movimentos robo completo
+void frenteRobot(){
+  frenteM1();
+  frenteM2();
+}
+void trazRobot(){
+  trazM1();
+  trazM2();
+}
+//motor1 frente motor2 traz direita
+void direitaRobot(){
+  frenteM1();
+  trazM2();
+  }
+void esquerdaRobot(){
+  trazM1();
+  frenteM2();
+  }
+
+
+    
---
+++ ponteH.ino
@@ -0,0 +1,32 @@
+
+/*Pinagem do arduino*/
+ 
+//motor A
+int IN1 = 2 ;//amarelo
+int IN2 = 3 ;//branco
+ 
+//motor B
+int IN3 = 4 ;//cinza
+int IN4 = 5 ;//roxo
+ 
+//Inicializa Pinos
+void setup(){
+ pinMode(IN1,OUTPUT);
+ pinMode(IN2,OUTPUT);
+ pinMode(IN3,OUTPUT);
+ pinMode(IN4,OUTPUT);
+}
+ 
+void loop(){
+//frenteM1(); //ok
+
+//freioM1();
+
+//trazM1();  //ok
+
+//frenteRobot(); //ok
+//trazRobot(); //ok
+//direitaRobot(); //ok
+//esquerdaRobot(); //ok
+ 
+}
